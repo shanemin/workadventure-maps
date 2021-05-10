687c8bdee8060dd1bd46e01d61abf7a39941ed57
Shane Minnema <shane.m@live.ca>
2021-05-10 12:33:17 +0200

    aapje test map

---
+++ Interiors_free_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Interiors_free_32x32" tilewidth="32" tileheight="32" tilecount="896" columns="16">
+ <image source="../../../Downloads/Modern_Interiors_Free_v2.1/Modern tiles_Free/Interiors_free/32x32/Interiors_free_32x32.png" width="512" height="1792"/>
+</tileset>
---
+++ open_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="open_tileset" tilewidth="32" tileheight="32" tilecount="6840" columns="90">
+ <image source="../../../Downloads/openpixels-master/openpixels-master/open_tileset_2x.png" width="2880" height="2432"/>
+</tileset>
